ad208a4f03bf2827ecc2a0fc17533691d8cdb4b0
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-06 15:46:58 +0530

    Power of 2 until 31

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Program to find 2 power till 31");
+int N = 0;
+while (N < 31)
+{
+    N++;
+    long power = (long)Convert.ToInt64(Math.Pow(2, N));
+    Console.WriteLine("The result of is : " + power);
+}
